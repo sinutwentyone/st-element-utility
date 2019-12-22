03b1f89ffce429ea538305bef708aa0035824ed9
Sinu Twentyone <sinutwentyone@gmail.com>
2019-12-22 23:42:30 +0700

    Update st-element-utility.js

--- st-element-utility.js
+++ st-element-utility.js
@@ -1,25 +1,35 @@
 (function( $, stLib, stIdentityObject ) {
-  var defaultOptions = {};
+  var defaultOptions = {
+    hideClass: 'st-hidden'
+  };
   
   if ( typeof window.stElementOptions !== 'function' ) { 
-    window.stElementOptions = function() {
+    window.stElementOptions = function( data, containerObj ) {
+      var obj = stBasicData( data );
       
+      obj.iORegister('stElementOptions');
+      
+      obj.getHideClass = function() {
+        return defaultOptions.hideClass;
+      };
+      
+      return obj;
     };
   } 
   
 })( jQuery, window.stLib, window.stIdentityObject );
 
-(function( $, stLib, stIdentityObject, ::stElemenOptions ) {
+(function( $, stLib, stIdentityObject, stElemenOptions ) {
   
   if ( typeof window.stElement !== 'function' ) {
     window.stElement = function( data, containerObj ) {
       var obj = stIdentityObject( null, containerObj ),
-          options = stLib.createData( data );
+          options = stElementOptions( data );
       
       obj.iORegister('stElement');
       
       obj._init = function() {
-        ::obj._setup$Element( options.element );
+        obj._setup$Element( options.data.element );
         
         return obj;
       };
@@ -36,12 +46,16 @@
         return Boolean( obj.get$Element.length );  
       };
       
+      obj.isHidden = function() {
+        return obj.hasClass( options.getHideClass() );
+      };
+      
       obj.hide = function() {
-        obj.addClass('st-hidden');
+        obj.addClass( options.getHideClass() );
       };
       
       obj.show = function() {
-        obj.removeClass('st-hidden');
+        obj.removeClass( options.getHideClass() );
       };
       
       return obj._init();       
@@ -49,3 +63,23 @@
   }
   
 })( jQuery, window.stLib, window.stIdentityObject, window.stElementOptions );
+
+(function( $, stLib, stIdentityObject ) {
+  if ( window.stElementTransitOptions !== 'function' ) {
+  
+  }  
+})( jQuery, window.stLib, window.stIdentityObject );
+
+(function( $, stLib, stIdentityObject, stElement ) {
+  if ( window.stElementTransit !== 'function' ) {
+    window.stElementTransit = function( data, containerObj ) {
+      var obj = stElement( data, containerObj );
+      
+      obj._init = function() {
+        return obj;
+      };
+      
+      return obj._init();
+    };   
+  }
+})( jQuery, window.stLib, window.stIdentityObject, window.stElement );
